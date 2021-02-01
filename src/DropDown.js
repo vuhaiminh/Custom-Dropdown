c32113e3b03535439066743285a5bd7af2f5bd38
unknown <vuhaiminh1308@gmail.com>
2021-01-31 20:00:07 -0600

    update package.json

---
+++ src/DropDown.js
@@ -0,0 +1,112 @@
+import React from 'react'
+import "./CSS/DropDown.css"
+import {IoIosArrowDown} from 'react-icons/io'
+import {IoIosArrowUp} from 'react-icons/io'
+import {IoIosCheckmark} from 'react-icons/io'
+import {IoIosCloseCircleOutline} from 'react-icons/io'
+class DropDown extends React.Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            open: false,
+            selectedItems: [] //state for all selected items
+        }
+    }
+    //function handle on click an item
+    handleSelectItem = (item) => {
+        const selection = this.state.selectedItems
+        if(!selection.some(current => current.id === item.id)){
+            selection.push(item)
+            this.setState({
+                selectedItems: selection
+            })
+        }else{
+            const removedSelection = selection.filter(current => current.id !== item.id)
+            this.setState({
+                selectedItems: removedSelection
+            })
+        }
+    }
+    //helper function to check if the clicked item is already selected
+    isItemInSelection = (item) => {
+        const selection = this.state.selectedItems
+        if (selection.find(current => current.id === item.id)){
+            return true;
+        }
+        return false;
+    }
+
+    //helper function to open/close the selection menu
+    toggle = (value) =>{
+        this.setState({
+            open: !value
+        })
+    }
+
+    render(){
+        const items = this.props.items
+        var selectedSomeItem = this.state.selectedItems.length > 0
+        return(
+         <div className="wrapper">
+             {/* Header Bar Start */}
+             <div
+             className="header"
+             tabIndex = {0}
+             role="button"
+             onClick={() => this.toggle(this.state.open)}
+            
+             >
+                 <div className="selectedItems">
+                     
+                     {
+                         selectedSomeItem ? 
+                         (
+                             <div className="pills-div">
+                             {this.state.selectedItems.map(item => (
+                                 <button className="pills" onClick={(e) => {
+                                    e.stopPropagation() 
+                                    this.handleSelectItem(item)}}>
+                                    <span className="selected-items-value">{item.value}</span>
+                                    <IoIosCloseCircleOutline />
+                                </button>
+                             ))}
+                             </div>
+                         )
+                         
+                         
+                         : <label className="selectIndicator">Movies</label>
+                     }
+                 </div>
+                 <div className="openIcon">
+                   {this.state.open ? <IoIosArrowUp /> : <IoIosArrowDown />}
+                 </div>
+                
+             </div>
+             {/* Header Bar Ends*/}
+            {/* Selection Menu Start Here */}
+            {this.state.open && (
+                <div className="listItems-div">
+                    <ul className="lists">
+                        {items.map(item => (
+                            <li className="listItem">
+                                <button  onClick={() => this.handleSelectItem(item)} className="selectItem-button">
+                                    <span>{item.value}</span>
+                                    <span> {this.isItemInSelection(item) && <IoIosCheckmark/> }</span>
+                                </button>
+                            </li>
+                        ))}
+                    </ul>
+                </div>
+
+            )}
+            {/* Selection Menu Ends Here */}
+             
+
+
+         </div>   
+        )
+    }
+
+}
+
+export default DropDown
---
+++ src/items.js
@@ -0,0 +1,66 @@
+const items = [
+    {
+        id: 1,
+        value: "Shrek"
+    },
+    {
+        id: 2,
+        value: "Wonder Woman"
+    },
+    {
+        id: 3,
+        value: "Casino Royale"
+    },
+    {
+        id: 4,
+        value: "King Kong"
+    },
+    {
+        id: 5,
+        value: "Friends"
+    },
+    {
+        id: 6,
+        value: "Harry Potter"
+    },
+    {
+        id: 7,
+        value: "Apollo 11"
+    },
+    {
+        id: 8,
+        value: "Godzilla"
+    },
+    {
+        id: 9,
+        value: "The Lego Movie"
+    },
+    {
+        id: 10,
+        value: "Naruto"
+    },
+    {
+        id: 11,
+        value: "2012"
+    },
+    {
+        id: 12,
+        value: "La La Land"
+    },
+    {
+        id: 13,
+        value: "Parasite"
+    }
+    ,
+    {
+        id: 14,
+        value: "Titanic"
+    },
+    {
+        id: 15,
+        value: "Spirited Away"
+    }
+    
+]
+
+export default items
